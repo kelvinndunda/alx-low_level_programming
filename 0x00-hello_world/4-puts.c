3c7aad8d77438a8a69f3d7fb6f97326a8fcd109d
kelvin ndunda <kelvinndunda001.gmail.com>
2023-03-16 16:26:44 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	char messenge[]="\"Programming is like building a multilingual puzzle\n";
-	puts(messenge);
+	char messenge[]="\"Programming is like building a multilingual puzzle";
+	puts(messenge\n);
 	return (0);
 }
