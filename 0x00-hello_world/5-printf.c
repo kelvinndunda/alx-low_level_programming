5eca49ee7d2c8646689fa025d9d970f04492f8c2
kelvin ndunda <kelvinndunda001.gmail.com>
2023-03-16 16:52:31 -0700

    Hello,printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char message[] = "with proper grammar, but the outcome is a piece of art,";
+	char *message = "with proper grammar, but the outcome is a piece of art,";
 
-	printf("%s", message);
+	printf("%s\n", message);
 	return (0);
 }
