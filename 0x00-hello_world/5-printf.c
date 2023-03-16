ad1e317beee443b5122a48aa618663e9387d6711
kelvin ndunda <kelvinndunda001.gmail.com>
2023-03-16 16:49:23 -0700

    Hello,printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,6 +7,8 @@
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	char message[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s", message);
 	return (0);
 }
