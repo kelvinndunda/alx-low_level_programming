381e8de28972aef6029a3e19b1965bff0826771f
kelvin ndunda <kelvinndunda001.gmail.com>
2023-03-16 17:21:25 -0700

    Size is not grander

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,10 @@
 
 int main(void)
 {
-	printf("Size of char:%c\n", sizeof(char));
-	printf("Size of an int: %d\n", sizeof(int));
-	printf("Size of a long int: %ld\n", sizeof(long int));
-	printf("Size of a long long int: %ld\n", sizeof(long long int));
-	printf("Size of a float: %f\n", sizeof(float));
-
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(double));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
