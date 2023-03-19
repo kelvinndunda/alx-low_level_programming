451f00370636fc13c01d327c9ed937c5fe5cf877
kelvin ndunda <kelvinndunda001.gmail.com>
2023-03-19 05:27:47 -0700

    alphabet game

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,8 +13,8 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
+		if (n > 0)
+		printf("Last digit of %d is positive\n", n);
 	else if (n < 0)
 		printf("%d is negative\n", n);
 	else if (n == 0)
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Return: 0 when successfull
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
