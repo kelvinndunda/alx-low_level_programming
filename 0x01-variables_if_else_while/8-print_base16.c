bfcd3c4c5ecd66278b88a9a5fd50d07fa917d459
kelvin ndunda <kelvinndunda001.gmail.com>
2023-03-19 13:11:28 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: 0 when successfull
+ */
+int main(void)
+{
+	int i;
+	int n;
+
+	for (n == 0; n <= 10; n++)
+	{
+		putchar(n);
+	}
+
+	for (i == 'a'; i < 'g'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
