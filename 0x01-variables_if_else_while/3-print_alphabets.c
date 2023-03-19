9e41221021df58372ec951e4a1b30d57b10bf124
kelvin ndunda <kelvinndunda001.gmail.com>
2023-03-19 05:41:39 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Return: 0 when successfull
+ */
+int main(void)
+{
+	int c;
+
+	for (c == 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+		for (c == 'A'; c <= 'Z'; c++)
+			putchar(c);
+	}
+	putchar('\n')
+	return (0);
+}
