d970dab8141c48e8446bfb889f80f0741efee81f
kelvin ndunda <kelvinndunda001.gmail.com>
2023-03-19 05:53:58 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,10 +8,10 @@ int main(void)
 {
 	int c;
 
-	for (c == 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
-		for (c == 'A'; c <= 'Z'; c++)
+		for (c = 'A'; c <= 'Z'; c++)
 			putchar(c);
 	}
 	putchar('\n');
