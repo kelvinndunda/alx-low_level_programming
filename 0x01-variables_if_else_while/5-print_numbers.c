8e7c8b298e917e58a57c56068a0fe27183e8188f
kelvin ndunda <kelvinndunda001.gmail.com>
2023-03-19 11:13:06 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: 0 when successfull
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d\n", n);
+		n++;
+	}
+	return (0);
+}
