c699a64adbc6d4b8350333fdf9cdaee922a9c9be
kelvin ndunda <kelvinndunda001.gmail.com>
2023-03-22 13:03:11 -0700

    question 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,44 +1,51 @@
+
 #include "main.h"
 
-/**
- * print_times_table - Prints times table of the input
- * Starting with 0
- * @n: The limit of row and collums
- */
 
-void print_times_table(int n)
-{
-	int num, mult, prod;
-
-	if (n >= 0 && n <= 15)
+	/**
+	 * print_times_table - prints the n times table, starting with 0
+	 * @n: number of the times table
+	 */
+	void print_times_table(int n)
 	{
-		for (num = 0; num <= n; num++)
+		int i, j, k;
+
+
+		if (n >= 0 && n <= 15)
 		{
-			_putchar('0');
-			for (mult = 1; mult <= n; mult++)
+			for (i = 0; i <= n; i++)
 			{
-				_putchar(',');
-				_putchar(' ');
-				prod = num * mult;
-				if (prod <= 99)
+				for (j = 0; j <= n; j++)
 				{
-					_putchar(' ');
+					k = j * i;
+					if (j == 0)
+					{
+						_putchar(k + '0');
+					} else if (k < 10 && j != 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(k + '0');
+					} else if (k >= 10 && k < 100)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar((k / 10) + '0');
+						_putchar((k % 10) + '0');
+					} else if (k >= 100)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar((k / 100) + '0');
+						_putchar(((k / 10) % 10) + '0');
+						_putchar((k % 10) + '0');
+					}
 				}
-				if (prod <= 9)
-				{
-					_putchar(' ');
-				}
-				if (prod >= 100)
-				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10) % 10) + '0');
-				} else if (prod <= 99 && prod >= 10)
-				{
-					_putchar((prod / 10) + '0');
-				}
-				_putchar((prod % 10) + '0');
+				_putchar('\n');
 			}
-			_putchar('\n');
 		}
 	}
-}
+
