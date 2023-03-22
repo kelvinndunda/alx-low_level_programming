c9d6d15c5a7c1a0ca8b87b23e550e2a6b18fbb86
kelvin ndunda <kelvinndunda001.gmail.com>
2023-03-22 12:50:39 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - function to add values
+ * @x: one of the int parameters
+ * @y: the onther int parameter
+ *
+ * Return: total of the integers entered
+ */
+
+int add(int x, int y)
+{
+	int total = x + y;
+
+	return (total);
+}
