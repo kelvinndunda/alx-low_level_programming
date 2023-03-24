33d391ea9accda0812e2d9c7aeb0c80261c48469
kelvin ndunda <kelvinndunda001.gmail.com>
2023-03-24 13:38:21 -0700

    1. isdigit' '

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,17 +3,16 @@
 /**
  * _isdigit - function to test
  * @c: test character
- * Return 1  when  digit, 0 otherwise
+ *description: checks for a digit (0 through 9)
+ * Return: 1  when  digit, 0 otherwise
  */
 
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
-	{
 		return (1);
-	}
+
 	else
-	{
 		return (0);
-	}
+
 }
