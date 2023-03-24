e8372f84f3b559f452dbc0ac8a3a8f2975f09ebb
kelvin ndunda <kelvinndunda001.gmail.com>
2023-03-24 15:21:29 -0700

    question 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - test function
+ *
+ *@n: number of times to draw
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
