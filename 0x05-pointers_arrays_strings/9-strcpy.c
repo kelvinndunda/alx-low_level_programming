24ed3ece21087fd48e858d0fa04ccfa56a5654aa
kelvin ndunda <kelvinndunda001.gmail.com>
2023-03-27 15:40:46 -0700

    function to cpy a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include"main.h"
+#include<string.h>
+
+/**
+ **_strcpy - this is copying a string
+ *@src: the string to be copyed
+ *@dest: the pointer to be pasted LOL
+ *
+ *Return: dest.
+ **/
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+
+	return (dest);
+}
