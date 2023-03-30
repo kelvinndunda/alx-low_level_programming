4ddb74944290e09a7239ca0e021de481a7de0e33
kelvin ndunda <kelvinndunda001.gmail.com>
2023-03-30 09:39:52 -0700

    concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  * *_strcat - appends string to another
+  * @dest: string destination
+  * @src: string source
+  * Return: returns destination
+  */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int destlen = 0;
+	int srclen = 0;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		destlen++;
+	for (i = 0; src[i] != '\0'; i++)
+		srclen++;
+	for (i = 0; i <= srclen; i++)
+		dest[destlen + i] = src[i];
+	return (dest);
+}
+
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0-strcat.c a program to concatinate two strings
