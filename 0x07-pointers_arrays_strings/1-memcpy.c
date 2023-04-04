5185b16f4b9856bd9efb512fce9afb7785d76b71
kelvin ndunda <kelvinndunda001.gmail.com>
2023-04-04 07:30:04 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * memset - test function
- * @dest: destination string
- * @src: source string
- * @n: integer
- * Return: pointer to dest
+ * _memcpy - copy n string from src to dest
+ *@dest: destination string
+ *@src: source string
+ *@n: number of bytes to copy
+ * Return: dest
  */
-
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
+
 	for ( ; i < n; i++)
 		dest[i] = '\0';
+
 	return (dest);
 }
